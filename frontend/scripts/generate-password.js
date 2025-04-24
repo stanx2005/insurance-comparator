33f1b6363332fd408731c428dcd571b21ded8fe9
stanx2005 <tahirred93@gmail.com>
2025-04-24 18:58:49 +0200

    feat: add password generation script

--- frontend/scripts/generate-password.js
+++ frontend/scripts/generate-password.js
@@ -1,10 +1,9 @@
 const bcrypt = require('bcryptjs')
 
-async function generateHash() {
-  const password = 'admin123' // Change this to your desired password
+async function generateHash(password) {
   const salt = await bcrypt.genSalt(10)
   const hash = await bcrypt.hash(password, salt)
-  console.log('Hashed password:', hash)
+  console.log('Password hash:', hash)
 }
 
-generateHash() 
+generateHash('admin123') 
